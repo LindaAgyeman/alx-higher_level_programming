36025d8e2f867b65692205bb874b08f2c51b071f
LindaAgyeman <lindaowusuagyeman89@gmail.com>
2023-12-19 08:48:35 -0800

    101-square

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+
+class Square:
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple) or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if not all(isinstance(val, int) for val in value) or value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        return self.size ** 2
+
+    def my_print(self):
+        if self.size == 0:
+            print()
+            return
+
+        for _ in range(self.position[1]):
+            print()
+
+        for _ in range(self.size):
+            print(" " * self.position[0] + "#" * self.size)
+
+    def __str__(self):
+        return self.my_print()
+
+if __name__ == "__main__":
+    my_square = Square(5, (0, 0))
+    print(my_square)
+
+    print("--")
+
+    my_square = Square(5, (4, 1))
+    print(my_square)
