5db1c768121fd8ad7741c5ef04ec500226d8072f
LindaAgyeman <lindaowusuagyeman89@gmail.com>
2024-03-28 06:58:32 -0700

    0-hbtn_status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,6 +1,15 @@
+#!/usr/bin/python3
+"""
+Script that fetches https://alx-intranet.hbtn.io/status
+"""
+
 import urllib.request
 
-if __name__ == "__main__":
+def fetch_status():
+    """
+    Fetches the status from https://alx-intranet.hbtn.io using urllib
+    and displays the body response.
+    """
     url = 'https://alx-intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as response:
         body = response.read()
@@ -8,3 +17,6 @@ if __name__ == "__main__":
         print("\t- type:", type(body))
         print("\t- content:", body)
         print("\t- utf8 content:", body.decode('utf-8'))
+
+if __name__ == "__main__":
+    fetch_status()
