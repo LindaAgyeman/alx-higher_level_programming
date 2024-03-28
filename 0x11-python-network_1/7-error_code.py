beb562d6ab4d5a5a876f5daea8ac5f19a1320cf9
LindaAgyeman <lindaowusuagyeman89@gmail.com>
2024-03-28 07:33:49 -0700

    7-error_code

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This script takes in a URL, sends a request to the URL, and displays
+the body of the response. If the HTTP status code is greater than or
+equal to 400, it prints an error message with the status code.
+"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    response = requests.get(url)
+    
+    if response.status_code >= 400:
+        print("Error code:", response.status_code)
+    else:
+        print(response.text)
